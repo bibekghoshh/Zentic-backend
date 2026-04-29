7c0d6ecf21d81564633a94cb8e2ef2bca10c9d70
bibekghoshh <bibek7478@gmail.com>
2026-04-29 21:34:36 +0530

    Migrate from nestJs to ExpressJS

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+// src/index.ts
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
